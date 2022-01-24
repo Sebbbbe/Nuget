456ed21469a5739a5a0f1f4d48717b4d1c20dd5d
Sebbbbe <sebastian.tiger901@gmail.com>
2022-01-25 00:33:54 +0100

    save 1

---
+++ CrudGeneric/Models/ModelGeneric.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrudGeneric.Models
+{
+    public class ModelGeneric
+    {
+        public int T1 { get; set; }
+        public string T2 { get; set; }
+
+        public List<string> T3 { get; set; }
+
+
+
+    }
+}
---
+++ CrudGeneric/Repository/IRepository/IRepositoryGeneric.cs
@@ -0,0 +1,14 @@
+﻿using CrudGeneric.Services.IService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrudGeneric.Repository.IRepository
+{
+    public interface IRepositoryGeneric<T> where T : class
+    {
+
+    }
+}
